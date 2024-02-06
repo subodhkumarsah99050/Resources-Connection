8c4e27b753222ea5d1616c7465a37f3285060e61
root <root@ip-10-181-19-182.us-east-2.compute.internal>
2024-02-06 13:49:32 +0000

    test

---
+++ network-interface-with-service.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+# Specify the network interface ID
+interface_id="eni-0c4941e7c06b9d961"
+
+# Check if the interface is attached to an EC2 instance
+attachment=$(aws ec2 describe-network-interfaces --network-interface-ids $interface_id --query 'NetworkInterfaces[*].Attachment.[InstanceId, DeviceIndex]' --output json)
+if [ "$attachment" != "[]" ]; then
+    echo "Interface $interface_id is attached to an EC2 instance: $attachment"
+else
+    echo "Interface $interface_id is not attached to any EC2 instance."
+fi
+
+# Check if the interface is associated with a load balancer
+load_balancer_arn=$(aws elbv2 describe-load-balancers --query "LoadBalancers[?contains(LoadBalancerName, '$interface_id')].LoadBalancerArn" --output text)
+if [ -n "$load_balancer_arn" ]; then
+    echo "Interface $interface_id is associated with a load balancer: $load_balancer_arn"
+else
+    echo "Interface $interface_id is not associated with any load balancer."
+fi
+
